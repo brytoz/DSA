824bdd6be1435e43853bdb957464e0e94b95ad3b
MAC <brytozee@yahoo.com>
2025-01-13 15:14:06 +0100

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+function isPalindrome(string) {
+  let x = 0;
+
+  const len = string.length 
+  for (x; len > x ; x++) {
+    if(string.length % 2 !== 0) {
+
+    }
+    console.log('wleomce');
+    console.log(string[x]);
+  }
+}
+
+
+  
+console.log(isPalindrome("madam")); // Output: true
+console.log(isPalindrome("hello")); // Output: false
+
